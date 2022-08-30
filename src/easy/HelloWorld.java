7a2b5f9668c99116975d3052dfa8eebac7ca43db
Oskar <oranhage@gmail.com>
2022-08-30 22:22:26 +0200

    Initial entry.

---
+++ src/easy/HelloWorld.java
@@ -0,0 +1,7 @@
+package easy;
+//Kattis problem: https://open.kattis.com/problems/hello
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
